58ee8c12e169484aeee703c20dd0018c67a5e5aa
prajwal0014 <prajwalkumar1777@gmail.com>
2025-08-16 19:23:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-  origin: 'https://kridhatrinkets.com',
+  origin: 'https://www.kridhatrinket.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
