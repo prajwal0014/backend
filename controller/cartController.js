a129aac8593d8e2df065ffa2689b9d1d5b2fe21e
prajwal0014 <prajwalkumar1777@gmail.com>
2025-08-26 12:07:23 +0530

    Update cartController.js

--- controller/cartController.js
+++ controller/cartController.js
@@ -28,7 +28,7 @@ export const addToCart = async (req,res) => {
       { new: true }
     );
 
-    return res.status(201).json({ message: "Added to cart", cartData: updatedUser.cartData});
+    return res.status(201).json({ message: "Added to cart"});
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: "addToCart error" });
